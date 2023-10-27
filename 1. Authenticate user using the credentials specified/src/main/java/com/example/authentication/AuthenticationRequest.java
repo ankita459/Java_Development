13ab468ade995b92a8f2037422e549c9a2250b97
Ankita Panpatil <ankitapanpatil18pc@student.mes.ac.in>
2023-10-27 16:45:20 +0530

    Add files via upload

---
+++ 1. Authenticate user using the credentials specified/src/main/java/com/example/authentication/AuthenticationRequest.java
@@ -0,0 +1,36 @@
+package com.example.authentication;
+
+public class AuthenticationRequest {
+	
+	private String loginId;
+    private String password;
+	/**
+	 * @return the loginId
+	 */
+	public String getLoginId() {
+		return loginId;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param loginId the loginId to set
+	 */
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+    // Getters and setters
+    
+    
+
+}
---
+++ 1. Authenticate user using the credentials specified/src/main/java/com/example/authentication/AuthenticationResponse.java
@@ -0,0 +1,25 @@
+package com.example.authentication;
+
+public class AuthenticationResponse {
+	
+	 private String token;
+
+	/**
+	 * @return the token
+	 */
+	public String getToken() {
+		return token;
+	}
+
+	/**
+	 * @param token the token to set
+	 */
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	    // Getters and setters
+	 
+	 
+
+}
